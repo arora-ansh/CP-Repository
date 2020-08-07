55e194cdbe60fb071ebea4d799752a659ecb35ba
Ansh Arora <55822413+arora-ansh@users.noreply.github.com>
2020-08-07 05:59:33 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stdio.h>
+#include <math.h>
+#include <iomanip>
+#include <queue> 
+#include <string.h>
+#include <string>
+using namespace std;
+
+inline void fastio(){
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  cout.tie(0);
+}
+
+typedef long long int ll;
+
+int main(){
+  fastio();
+  int T;
+  cin>>T;
+  for(int t=0;t<T;t++){
+  //logic here  mostly lol
+  }
+  return 0;
+}
+
+
