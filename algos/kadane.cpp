8ec61ac1263126b0b1aa427753fe4c28adfd63fd
t-i-t-a-n <t-i-t-a-n@users.noreply.github.com>
2020-08-08 05:46:13 +0530

    New files

---
+++ algos/kadane.cpp
@@ -0,0 +1,21 @@
+//Kadane's Algorithm for finding max sum possible from contiguous arrays
+//DP
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[8] = {-2,-3,4,-1,-2,1,-2,5,-3};
+	int global_max = 0;
+	int curmax = 0;
+	for(int i=0;i<9;i++){
+		curmax = curmax + arr[i];
+		if(curmax>global_max){
+			global_max = curmax;
+		}
+		if(curmax<0){
+			curmax = 0;
+		}
+	}
+	cout<<global_max<<endl;
+	return 0;
+}
