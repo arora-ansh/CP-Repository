94d7b9f4d2fe9ea92d7b43e64a82b28bd1e5de77
t-i-t-a-n <t-i-t-a-n@users.noreply.github.com>
2020-09-08 05:05:53 +0530

    Update files

--- algos/segmentTrees.cpp
+++ algos/segmentTrees.cpp
@@ -12,6 +12,40 @@ void buildTree(int *arr, int *tree, int start, int end, int treeNode){
 	tree[treeNode] = tree[2*treeNode]+tree[2*treeNode+1];
 }
 
+void updateTree(int *arr, int *tree, int start, int end, int treeNode, int index, int value){
+
+	if(start==end){
+		arr[index] = value;
+		tree[treeNode] = value;
+		return;
+	}
+	int mid = (start+end)/2;
+	if(index>mid){
+		updateTree(arr,tree,mid+1,end,2*treeNode+1,index,value);
+	}
+	else{
+		updateTree(arr,tree,start,mid,2*treeNode,index,value);
+	}
+	tree[treeNode] = tree[2*treeNode]+tree[2*treeNode+1];
+}
+
+int queryTree(int *tree,int start,int end,int treeNode, int rangeLeft, int rangeRight){
+
+	//Outside Range
+	if(start > rangeRight || end < rangeLeft){
+		return 0;
+	}
+	//Inside Range
+	if(start >= rangeLeft && end <= rangeRight){
+		return treeNode;
+	}
+	int mid = (start+end)/2;
+	int leftSum = queryTree(tree,start,mid,2*treeNode,rangeLeft,rangeRight);
+	int rightSum = queryTree(tree,mid+1,end,2*treeNode+1,rangeLeft,rangeRight);
+
+	return leftSum+rightSum;
+}
+
 int main(){
 	int arr[] = {1,2,3,4,5,6,7,8,9};
 	int n = 9; //Size of array
@@ -22,6 +56,11 @@ int main(){
 	for(int i=1;i<2*n;i++){
 		cout<<tree[i]<<" ";
 	}
-	cout<<endl;
+	cout<<endl<<endl;
+	updateTree(arr,tree,0,8,1,3,12);
+	int ans = queryTree(tree,0,8,1,2,5);//In this example, summing between the given range
+	cout<<ans<<endl;
 	return 0;
 }
+
+
